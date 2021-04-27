52cff43549634bf3a99f111711c6cf28f0c90339
Rita Li <61838154+Rita-Li527@users.noreply.github.com>
2021-04-27 16:43:17 -0500

    update bubble animation

--- Assets/cokeAudio.cs
+++ Assets/cokeAudio.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class cokeAudio : MonoBehaviour
 {
     public AudioSource src;
+    //int n = 0;
+
+    // public Button button;
     // Start is called before the first frame update
     void Start()
     {
+        //if (n >= 1){
+        //   button.interactable = false;
+        //}
         
     }
 
@@ -20,6 +27,9 @@ public class cokeAudio : MonoBehaviour
     public void PlayMusic()
     {
         src.Play();
+        //n = n+1;
+       
+        
 
     }
 }
